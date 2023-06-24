adaedf419376c9f45b1b78273a00e3eca2f6f15b
Ritik Bhateley <ritikbhateley125@gmail.com>
2023-06-24 22:02:11 +0530

    Contact Section Added

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ContactWrapper, Email, LinkedIn } from "./ContactElements";
+import { FaLinkedin,FaGithub } from "react-icons/fa";
+
+
+function Contact() {
+  return (
+    <ContactWrapper id="contact">
+      <div className="Container">
+        <div className="SectionTitle">Get In Touch</div>
+        <div className="BigCard">
+          <Email>
+            <span>ritikbhateley@gmail.com</span>
+            <a
+              className="btn PrimaryBtn"
+              href="mailto:ritikbhateley@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send Mail
+            </a>
+          </Email>
+          <LinkedIn>
+            <a
+              href="https://www.linkedin.com/in/ritik-bhateley/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/ritik-078/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+          </LinkedIn>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default Contact;
---
+++ src/components/Contact/ContactElements.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const ContactWrapper = styled.div`
+  margin: 5rem 0;
+`;
+
+export const Email = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
+
+  span {
+    font-size: 1rem;
+    font-weight: 400;
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: 576px) {
+    span {
+      font-size: 1.5rem;
+    }
+  }
+  @media (min-width: 992px) {
+    flex-direction: row;
+    span {
+      margin-bottom: 0;
+      font-size: 3rem;
+    }
+  }
+`;
+
+export const LinkedIn = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 3rem;
+  a {
+    font-size: 2rem;
+    color: #000009;
+    transition: 0.2s ease-in;
+    margin-top: 2rem;
+    margin-right: 3rem;
+    &:hover {
+      color: rgb(57, 134, 250);
+    }
+  }
+  
+`;
