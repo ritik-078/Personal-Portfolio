0b4b77f02b43f9c1d92ad8701af3dffa595c3503
Ritik Bhateley <ritikbhateley125@gmail.com>
2023-06-24 22:41:20 +0530

    Project Section Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import Main from "../components/Main/Main";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
+import Projects from "../components/Projects/Projects";
 
 function Home() {
   return (
     <div>
       <Main />
+      <Projects />
       <About />
       <Contact />
       <Footer />
